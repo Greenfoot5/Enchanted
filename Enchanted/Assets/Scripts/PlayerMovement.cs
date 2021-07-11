cca5ac207e3ac65224ae9cf7f3f996b97a5fb7d3
Greenfoot5 <greenfoot89@gmail.com>
2021-07-11 16:38:06 +0100

    Added basic rotation as movement. Also move the deadZone variable

--- Enchanted/Assets/Scripts/PlayerMovement.cs
+++ Enchanted/Assets/Scripts/PlayerMovement.cs
@@ -2,33 +2,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float moveSpeed = 3f;
+    public float rotationSpeed = 90f;
+    
+    [Header("Joystick Settings")]
     public Joystick joystick;
+    public float deadZone = 0.2f;
+    
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = new Vector3();
-        
+
         // Movement in the X axis
-        if (joystick.Horizontal > 0)
+        if (joystick.Horizontal > deadZone)
         {
             movement.x = moveSpeed;
         }
-        else if (joystick.Horizontal < 0)
+        else if (joystick.Horizontal < -deadZone)
         {
             movement.x = moveSpeed * -1;
         }
         
         // Movement in the Y axis
-        if (joystick.Vertical > 0)
+        if (joystick.Vertical > deadZone)
         {
             movement.z = moveSpeed;
         }
-        else if (joystick.Vertical < 0)
+        else if (joystick.Vertical < -deadZone)
         {
             movement.z = moveSpeed * -1;
         }
-
-        gameObject.transform.Translate(movement * Time.deltaTime);
+        
+        
+        gameObject.transform.Translate(movement * Time.deltaTime, Space.World);
+        
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 
+                rotationSpeed * Time.deltaTime); 
+        }
     }
 }
