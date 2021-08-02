4bd3fb22694fd3f8d9bb9f681883e3e304340bd3
Greenfoot5 <greenfoot89@gmail.com>
2021-08-02 15:54:27 +0100

    Implemented RangeInt and improved RangeFloat, to only show slider when there is a MinMaxRange Attribute

---
+++ Enchanted/Assets/Abstract/Ranges/RangedInt.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public struct RangedInt
+{
+    public int minValue;
+    public int maxValue;
+}
--- Enchanted/Assets/Abstract/Ranges/RangedIntDrawer.cs
+++ Enchanted/Assets/Abstract/Ranges/RangedIntDrawer.cs
@@ -1,9 +1,8 @@
-using System;
 using UnityEngine;
 using UnityEditor;
 
-[CustomPropertyDrawer(typeof(RangedFloat), true)]
-public class RangedFloatDrawer : PropertyDrawer {
+[CustomPropertyDrawer(typeof(RangedInt), true)]
+public class RangedIntDrawer : PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         label = EditorGUI.BeginProperty(position, label, property);
@@ -12,31 +11,34 @@ public class RangedFloatDrawer : PropertyDrawer {
         var minProp = property.FindPropertyRelative("minValue");
         var maxProp = property.FindPropertyRelative("maxValue");
 
-        float[] range = {minProp.floatValue, maxProp.floatValue};
+        int[] range = {minProp.intValue, maxProp.intValue};
 
         var ranges = (MinMaxRangeAttribute[])fieldInfo.GetCustomAttributes(typeof (MinMaxRangeAttribute), true);
         if (ranges.Length > 0)
         {
-            var rangeMin = ranges[0].Min;
-            var rangeMax = ranges[0].Max;
+            var rangeMin = (int) ranges[0].Min;
+            var rangeMax = (int) ranges[0].Max;
 
             const float rangeBoundsLabelWidth = 40f;
 
             var rangeBoundsLabel1Rect = new Rect(position) {width = rangeBoundsLabelWidth};
-            GUI.Label(rangeBoundsLabel1Rect, new GUIContent(range[0].ToString("F2")));
+            GUI.Label(rangeBoundsLabel1Rect, new GUIContent(rangeMin.ToString()));
             position.xMin += rangeBoundsLabelWidth;
 
             var rangeBoundsLabel2Rect = new Rect(position);
             rangeBoundsLabel2Rect.xMin = rangeBoundsLabel2Rect.xMax - rangeBoundsLabelWidth;
-            GUI.Label(rangeBoundsLabel2Rect, new GUIContent(range[1].ToString("F2")));
+            GUI.Label(rangeBoundsLabel2Rect, new GUIContent(rangeMax.ToString()));
             position.xMax -= rangeBoundsLabelWidth;
+            
+            GUIContent[] subLabels = {GUIContent.none, GUIContent.none};
 
             EditorGUI.BeginChangeCheck();
-            EditorGUI.MinMaxSlider(position, ref range[0], ref range[1], rangeMin, rangeMax);
+            EditorGUI.MultiIntField(position, subLabels, range);
             if (EditorGUI.EndChangeCheck())
             {
-                minProp.floatValue = range[0];
-                maxProp.floatValue = range[1];
+                // Make sure both values stay between the range
+                minProp.intValue = range[0] < rangeMin || range[0] > rangeMax ? rangeMin : range[0];
+                maxProp.intValue = range[1] < rangeMin || range[1] > rangeMax ? rangeMax : range[1];
             }
         }
         else
@@ -44,11 +46,11 @@ public class RangedFloatDrawer : PropertyDrawer {
             GUIContent[] subLabels = {GUIContent.none, GUIContent.none};
             
             EditorGUI.BeginChangeCheck();
-            EditorGUI.MultiFloatField(position, subLabels, range);
+            EditorGUI.MultiIntField(position, subLabels, range);
             if (EditorGUI.EndChangeCheck())
             {
-                minProp.floatValue = (float) Math.Round(range[0], 5);
-                maxProp.floatValue = (float) Math.Round(range[1], 5);
+                minProp.intValue = range[0];
+                maxProp.intValue = range[1];
             }
         }
 
